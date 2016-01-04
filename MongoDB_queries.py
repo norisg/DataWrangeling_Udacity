fff7f73047ca7ddb61dfa0a92cba0479481b6142
norisg <noritno99@gmail.com>
2016-01-04 20:13:23 +0100

    Corrections after 1 Submission

--- MongoDB_queries.py
+++ MongoDB_queries.py
@@ -35,15 +35,63 @@ def make_pipeline():
     pipeline5 = [{"$match":{"address.street":{"$exists":1}}}, {"$group":{"_id":"$address.street", "count":{"$sum":1}}}, {"$sort":{'count' :-1}}]
     pipeline6 = [{"$group":{"_id":"$created.uid", "count":{"$sum":1}}}, {'$sort' : {'count' :-1}}, {"$limit":1}]
     pipeline7 = [{"$match":{"amenity":{"$exists":1}, "amenity":"restaurant", "cuisine" : {"$exists":1}}}, {"$group":{"_id":"$cuisine", "count":{"$sum":1}}},{'$sort' : {'count' :-1}}, {"$limit":3}]
-    return pipeline7
+
+   # Auditing the data 
+   # Checking for City names  
+    pipeline_city_names = [{"$match" : {"address.city" : {"$exists" : 1}}},
+                           {"$group" : {"_id" : "$address.city", "count" : {'$sum' : 1}}},
+                            {"$sort" : {"count" : 1}}]
+    pipeline_amenties = [{"$match" : {"amenity" : {"$exists" : 1}}},
+                           {"$group" : {"_id" : "$amenity", "count" : {'$sum' : 1}}},
+                            {"$sort" : {"count" : 1}}]
+    return pipeline_amenties
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------    
-if __name__ == '__main__':
+def city_name():
+     city_re = re.compile (r'Aix en Provence$', re.IGNORECASE)
+     db = get_db('DataWrangeling')
+     match = db.Aix_en_Provence.update_many(
+     {"address.city" : { '$regex' : city_re}},
+     {'$set' :
+     {"address.city" : 'Aix-en-Provence'}})
+     print match
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+def upadate_cedex():
     db = get_db('DataWrangeling')
-    list = defaultdict(set)
-    street_type_re = re.compile(r'^\b\S+\.?', re.IGNORECASE)
+    cedex_re = re.compile (r'Cedex', re.IGNORECASE)
+    cedex = db.Aix_en_Provence.find({"address.city" : { '$regex' : cedex_re}})
+    print cedex.count()
+    for r in cedex:
+         r["address"]["city"] = cedex_re.sub("CEDEX",r["address"]["city"]) 
+         print r["address"]["city"]
+         db.Aix_en_Provence.update({'_id': r['_id']},r);
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+def update_amenities():
+     db = get_db('DataWrangeling')
+     amenities_re = re.compile (r'_', re.IGNORECASE)
+     amenity = db.Aix_en_Provence.find({"amenity" : { '$regex' : amenities_re}}) 
+     
+     print amenity
+     for r in amenity:
+         print r["amenity"]
+         r["amenity"] = amenities_re.sub(" ",r["amenity"]) 
+         db.Aix_en_Provence.update({'_id': r['_id']},r);
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+   
+    #list = defaultdict(set)
+    #street_type_re = re.compile(r'^\b\S+\.?', re.IGNORECASE)
+    #city_name()
+    #upadate_cedex()
+    update_amenities()
     array = aggregate(db, make_pipeline())
-    print array
+    pprint.pprint(array)
+    #update()
+   
 
   
     
